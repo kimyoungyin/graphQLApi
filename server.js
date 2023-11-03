023ee7c6ee4945150099a9a27a8a26d17695cd4f
kimyoungyin <yin199859@gmail.com>
2023-11-03 16:28:55 +0900

    apollo-server test with simple javascript object

--- server.js
+++ server.js
@@ -1,12 +1,56 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const user = {
+    firstName: "yeongyin",
+    lastName: "Kim",
+    age: 26,
+};
+
 const typeDefs = gql`
+    type User {
+        firstName: String!
+        lastName: String!
+        fullName: String!
+        age: Int!
+    }
     type Query {
-        text: String
+        author: User!
+    }
+    type Mutation {
+        gettingOld: User!
+        gettingYoung: User!
+        resetAge: User!
     }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+    Query: {
+        author() {
+            return user;
+        },
+    },
+    Mutation: {
+        gettingOld() {
+            user.age++;
+            return user;
+        },
+        gettingYoung() {
+            user.age--;
+            return user;
+        },
+        resetAge() {
+            user.age = 26;
+            return user;
+        },
+    },
+    User: {
+        fullName({ firstName, lastName }) {
+            return `${firstName} ${lastName}`;
+        },
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`running on ${url}`);
